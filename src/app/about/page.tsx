850665991cb2bc4cf398d56fe77311837a2d5d81
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-03-13 16:31:03 +0100

    feat: about page particles update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,12 @@
 import AboutLogo from "../_components/about-logo";
 
 export default async function About() {
-  return <AboutLogo />;
+  return <>
+    <AboutLogo />
+    <div className="container">
+      <p>
+        This is a simple recipe manager.
+      </p>
+    </div>
+  </>;
 }
