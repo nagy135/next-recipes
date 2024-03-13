30af6dbf07163e8a09d522330bc8395d11883dd6
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-03-13 21:31:19 +0100

    feat: ui find cards

--- src/app/_components/list-recipes.tsx
+++ src/app/_components/list-recipes.tsx
@@ -1,5 +1,14 @@
 import { Separator } from "~/components/ui/separator";
 import { type RecipeWithIngredients } from "~/types";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card"
+import { Button } from "./ui/button";
 
 type ListRecipesProps = {
   records: RecipeWithIngredients[];
@@ -7,23 +16,33 @@ type ListRecipesProps = {
 
 export function ListRecipes({ records }: ListRecipesProps) {
   return (
-    <>
+    <div className="flex flex-col gap-3 md:grid md:grid-cols-2 lg:grid-cols-3">
       {records.map((record) => (
         <>
-          <div key={record.recipe?.id}>
-            <h1>{record.recipe?.name}</h1>
-            <h1>Ingredients:</h1>
-            <ul>
-              {record.ingredients.map((ingredient) => (
-                <li key={ingredient.id}>
-                  {ingredient.amount} {ingredient.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <Separator />
+          <Card>
+            <CardHeader>
+              <CardTitle>{record.recipe?.name ?? '-'}</CardTitle>
+              <CardDescription>by {record.recipe?.userId}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div key={record.recipe?.id}>
+                <h1>{record.recipe?.name}</h1>
+                <h1>Ingredients:</h1>
+                <ul>
+                  {record.ingredients.map((ingredient) => (
+                    <li key={ingredient.id}>
+                      {ingredient.amount} {ingredient.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">View</Button>
+            </CardFooter>
+          </Card>
         </>
       ))}
-    </>
+    </div>
   );
 }
